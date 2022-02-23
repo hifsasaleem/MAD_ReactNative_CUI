6ff6a37fa7fc00e9aaaf33c5e606fdfe22a57588
Hifsa <pariq246@gmail.com>
2022-02-23 20:56:56 +0500

    .

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -7,3 +7,7 @@ console.log(b);
 
 var someNumber=45;
 console.log(someNumber);
+
+document.getElementById('someText').innerHTML = "Hey There";
+let age = prompt("What is your age");
+document.getElementById('someHeading').innerHTML = age;
