2cc3b3bbc22323ff08a58825ddb8fa414e4dca56
Hifsa <pariq246@gmail.com>
2022-02-23 21:42:53 +0500

    .

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -8,6 +8,27 @@ console.log(b);
 var someNumber=45;
 console.log(someNumber);
 
-document.getElementById('someText').innerHTML = "Hey There";
-let age = prompt("What is your age");
-document.getElementById('someHeading').innerHTML = age;
+//Manipulate DOM with JavaScript
+/* changing HTML with JavaScript*/
+
+//document.getElementById('someText').innerHTML = "Hey There";
+//let age = prompt("What is your age");
+//document.getElementById('someHeading').innerHTML = age;
+
+//Numbers in JavaScript
+ var num1 = 5;
+ //increment num1 by 1
+ num1++;
+  //decrament num1 by 1
+ num1--;
+  console.log(num1);
+
+  // Divide ,multiply * , remainder %
+  console.log(num1 + " + " + " 6 =" ,num1 + 6);
+  console.log(num1 + " - " + " 6 =" ,num1 - 6);
+  console.log(num1 + " * " + " 6 =" ,num1 * 6);
+  console.log(num1 + " / " + " 6 =" ,num1 / 6);
+  console.log(num1 + " % " + " 6 =" ,num1 % 6);
+  
+
+
